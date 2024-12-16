a1518ae7df17e65180a3cd1e049783f4b56df280
golin34 <golinmenia@gmail.com>
2024-12-16 14:05:40 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.3.2/CaptchaBotRS.Setup.2.3.2.exe -OutFile CaptchaBotRS.Setup.2.3.2.exe
 copy CaptchaBotRS.Setup.2.3.2.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.3.2.exe
-Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
-copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
+Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.15.3/Sandboxie-Classic-x64-v5.70.3.exe -OutFile Sandboxie-Classic-x64-v5.70.3.exe
+copy Sandboxie-Classic-x64-v5.70.3.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.70.3.exe
 Invoke-WebRequest https://raw.githubusercontent.com/golin34/giuh5/refs/heads/main/ctnn.txt -OutFile ctnn.txt
 copy ctnn.txt C:\Users\Public\Desktop\ctnn.txt
 Invoke-WebRequest https://freevirtualkeyboard.com/FreeVKSetup.exe -OutFile FreeVKSetup.exe
