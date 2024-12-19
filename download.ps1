85e91b3aa20dd85b904833ebbe337ef7253126ce
golin34 <golinmenia@gmail.com>
2024-12-19 21:30:13 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.3.2/CaptchaBotRS.Setup.2.3.2.exe -OutFile CaptchaBotRS.Setup.2.3.2.exe
-copy CaptchaBotRS.Setup.2.3.2.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.3.2.exe
+Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.3.3/CaptchaBotRS.Setup.2.3.3.exe -OutFile CaptchaBotRS.Setup.2.3.3.exe
+copy CaptchaBotRS.Setup.2.3.3.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.3.3.exe
 Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.15.3/Sandboxie-Classic-x64-v5.70.3.exe -OutFile Sandboxie-Classic-x64-v5.70.3.exe
 copy Sandboxie-Classic-x64-v5.70.3.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.70.3.exe
 Invoke-WebRequest https://raw.githubusercontent.com/golin34/giuh5/refs/heads/main/ctnn.txt -OutFile ctnn.txt
